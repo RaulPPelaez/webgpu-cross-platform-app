52219617e8143f7f7c54b67f4e12ef06a08b5a35
RaulPPealez <raul.perez@uam.es>
2025-08-17 14:28:58 +0200

    build: better emscripten installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,10 @@
 conda env update -n wgpu
 source activate base
 conda activate wgpu
-# Clone only if necessary
-if [ ! -d "emsdk" ]; then
-    git clone https://github.com/emscripten-core/emsdk.git
-else
-    (cd emsdk && git pull)
-
-fi
-cd emsdk
-./emsdk install $EMSCRIPTEN_VERSION
-./emsdk activate $EMSCRIPTEN_VERSION
+emsdk update
+emsdk install $EMSCRIPTEN_VERSION
+emsdk activate $EMSCRIPTEN_VERSION
 CONDA_PREFIX=$(conda info --base)/envs/wgpu
 mkdir -p $CONDA_PREFIX/etc/conda/activate.d
 echo "#!/bin/sh" > $CONDA_PREFIX/etc/conda/activate.d/emscripten.sh
-echo "source $PWD/emsdk_env.sh" >> $CONDA_PREFIX/etc/conda/activate.d/emscripten.sh
+echo "source $(python -c 'import site; print(site.getsitepackages()[0])')/emsdk/emsdk_env.sh" >> $CONDA_PREFIX/etc/conda/activate.d/emscripten.sh
